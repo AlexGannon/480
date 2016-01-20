45107afdf00dafe3fa65888a97cd98c3856d9ddc
L <wpplefka@oakland.edu>
2016-01-19 20:53:10 -0500

    Updated Shell

--- source/src/main/java/android/com/freezeframe/LoginActivity.java
+++ source/src/main/java/android/com/freezeframe/LoginActivity.java
@@ -5,16 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
     Button button = null;
-
+    TextView registerTv = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         button = (Button) findViewById(R.id.button);
+        registerTv = (TextView) findViewById(R.id.register);
+
+
+        registerTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), RegisterActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
--- source/src/main/java/android/com/freezeframe/RegisterActivity.java
+++ source/src/main/java/android/com/freezeframe/RegisterActivity.java
@@ -5,9 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
     Button button = null;
+    TextView loginTv = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,17 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         button = (Button) findViewById(R.id.button);
+        loginTv = (TextView) findViewById(R.id.login);
+
+
+        loginTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
